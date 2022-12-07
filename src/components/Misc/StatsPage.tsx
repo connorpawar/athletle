73fb2b36b6ce6ca3bcb73e3e302de100f796feaa
unknown <connorpawar@gmail.com>
2022-12-06 20:19:40 -0600

    ATH-013 : fix the storage problems

--- src/components/Misc/StatsPage.tsx
+++ src/components/Misc/StatsPage.tsx
@@ -1,6 +1,6 @@
 import type { ReactElement} from "react";
 import { useEffect, useState } from "react";
-import type { StoredGuesses } from "../Game/GameBoard";
+import type { HistoricalData } from "../Game/GameBoard";
 import { GameStatsCard } from "../Game/GameStatsCard";
 import { useSportContext } from "~/contexts/SportContext";
 import { useLocalStorage } from "~/hooks/useLocalStorage";
@@ -10,21 +10,18 @@ import { mapReplacer, mapReviver } from "~/utils/mapHelpers";
 export function StatsPage(): ReactElement {
     const { sportsLeague } = useSportContext();
 
-    const defaultStoredGuesses: StoredGuesses = {
-        date: new Date().toLocaleDateString(),
-        latestPlayerId: "",
+    const defaultHistoricalData: HistoricalData = {
         leagueId: sportsLeague.id,
-        latestGuesses: [],
         gamesPlayed: 0,
         currentStreak: 0,
         guessDistribution: new Map<number, number>(),
     };
 
-    const [storedData] = useLocalStorage(`storedData-${sportsLeague.id}`, JSON.stringify(defaultStoredGuesses, mapReplacer));
-    const [data, setData] = useState<StoredGuesses>(defaultStoredGuesses);
+    const [storedData] = useLocalStorage(`historicalData-${sportsLeague.id}`, JSON.stringify(defaultHistoricalData, mapReplacer));
+    const [data, setData] = useState<HistoricalData>(defaultHistoricalData);
     
     useEffect(() => {
-        const guessData = JSON.parse(storedData, mapReviver) as StoredGuesses;
+        const guessData = JSON.parse(storedData, mapReviver) as HistoricalData;
         setData(guessData);
     }, [storedData]);
     
